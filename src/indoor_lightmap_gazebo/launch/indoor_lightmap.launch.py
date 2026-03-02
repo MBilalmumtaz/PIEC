d46a9cad162eed69b0f0a1cfd0f6e530a71ac982
Engramjad321 <79195094+Engramjad321@users.noreply.github.com>
2026-03-02 12:44:30 +0800

    Add files via upload

---
+++ src/indoor_lightmap_gazebo/launch/indoor_lightmap.launch.py
@@ -0,0 +1,29 @@
+import os
+from launch import LaunchDescription
+from launch.actions import SetEnvironmentVariable, IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    pkg_share = FindPackageShare('indoor_lightmap_gazebo').find('indoor_lightmap_gazebo')
+    world_file = os.path.join(pkg_share, 'worlds', 'indoor_lightmap.world')
+
+    gazebo_resource_path = SetEnvironmentVariable(
+        name='GZ_SIM_RESOURCE_PATH',
+        value=os.path.join(pkg_share, 'models') + ':' +
+              os.path.join(pkg_share, 'worlds')
+    )
+
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [FindPackageShare('ros_gz_sim'), '/launch/gz_sim.launch.py']
+        ),
+        launch_arguments={
+            'gz_args': ['-r ', world_file]
+        }.items()
+    )
+
+    return LaunchDescription([
+        gazebo_resource_path,
+        gazebo,
+    ])
